950c353a306bb1884b9794ae72a18285f877b0d0
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-04 15:07:43 -0700

    Hardcode beers

---
+++ src/assets/BeerData.js
@@ -0,0 +1,56 @@
+export const beers = [
+    {
+       name: "The Funky Drummer",
+       brand: "Funhouse Brews",
+       type: "Belgian",
+       price: "6",
+       ibu: "36",
+       abv: "5.5",
+       description: "Farmhouse style; Modestly hopped with Loral hops"
+    },
+    {
+       name: "Brut IPA",
+       brand: "Deschutes",
+       type: "Brut IPA",
+       price: "6",
+       ibu: "35",
+       abv: "6.4",
+       description: "A bone-dry IPA swirling with fruity, white wine-like aromas and enticing effervescence"
+    },
+    {
+       name: "Bruticus Citricus",
+       brand: "Funhouse Brews",
+       type: "Brut IPA",
+       price: "7",
+       ibu: "54",
+       abv: "6.0",
+       description: "An easy drinking Brut IPA, highly carbonated showcase of citrus-forward hops"
+    },
+    {
+       name: "Sour Flower",
+       brand: "Culmination",
+       type: "Gose & Sour",
+       price: "6",
+       ibu: "5",
+       abv: "5.5",
+       description: "Sour blonde ale with raspberry & lime zest"
+    },
+    {
+       name: "Young Squeezy",
+       brand: "10 Barrel",
+       type: "Hazy IPA",
+       price: "5",
+       ibu: "50",
+       abv: "6.2",
+       description: "Soft malt, low IBU & huge tropical hop profile	"
+    },
+    {
+       name: "Zoigl-Schwarts",
+       brand: "Zoiglhaus",
+       type: "Lager",
+       price: "6",
+       ibu: "24",
+       abv: "4.8",
+       description: "A dark lager with clean, light malt sweetness from the Pilsner and Munich malts	"
+    }
+   ];

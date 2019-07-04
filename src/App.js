3b03d813488adfd5ac3d31b104c6860f0b54469a
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-04 11:13:57 -0700

    Add and import styling to NavBar in app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar'
+import NavBar from './NavBar';
+import 'mdbreact/dist/css/mdb.css';
 
 function App() {
   return (

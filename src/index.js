3718c307c72add38b5f8230c26e89bd0e8f80fc7
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-04 12:09:21 -0700

    Import mdb and fontawesome elements into index to fix styling issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
+import "./index.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 

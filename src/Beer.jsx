34468f2129961f6e7a9d7174ca26e256c2f27f0e
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-04 16:01:23 -0700

    Add Beer component, add PropTypes

---
+++ src/Beer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function Beer(props){
+  return (
+    <div>
+      <h2><em>{props.brand}</em>{props.name}</h2>
+      <h4>{props.type}</h4>
+      <p>$ {props.price} | abv: {props.abv} | ibu: {props.ibu}</p>
+      <p><em>{props.description}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Beer.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  type: PropTypes.string,
+  price: PropTypes.number,
+  abv: PropTypes.number,
+  ibu: PropTypes.number,
+  description: PropTypes.string
+};
+
+export default Beer;

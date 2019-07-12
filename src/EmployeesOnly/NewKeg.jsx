99d82a726adc65515995f22a8b9f0ec0c423fae1
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-12 16:03:38 -0700

    Add Employee Login Control

--- src/EmployeesOnly/NewKeg.jsx
+++ src/EmployeesOnly/NewKeg.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
-function NewBeer() {
-
+function NewKeg(){
   return (
-    <h1><
+    <div>
+      <h1>Add a beer to the list:</h1>
+      <form>
+        <input
+          type='text'
+          id='name'
+          placeholder='Beer name'/><br/><br/>
+
+        <input
+          type='text'
+          id='type'
+          placeholder='Beer type'/><br/><br/>
+
+        <input
+          type='number'
+          id='ABV'
+          placeholder='ABV'/><br/><br/>
+
+        <input
+          type='number'
+          id='price'
+          placeholder='Beer price'/><br/><br/>
+
+        <textarea
+          id='description'
+          placeholder='Beer description.'/><br/><br/>
+
+        <button type='submit'>Add new beer</button>
+      </form>
+    </div>
   );
 }
 
-export default NewBeer;
+export default AddKeg;
+
+name: "Sour Flower",
+brand: "Culmination",
+type: "Gose & Sour",
+price: "6",
+ibu: "5",
+abv: "5.5",
+description: "Sour blonde ale with raspberry & lime zest",
+pints: "124"

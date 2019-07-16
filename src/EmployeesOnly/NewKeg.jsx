1e20bc3d8490771da2138790afda74aa612f0923
Lindsey Baker <lindsey.baker0@gmail.com>
2019-07-16 07:28:24 -0700

    Add refs to forms

--- src/EmployeesOnly/NewKeg.jsx
+++ src/EmployeesOnly/NewKeg.jsx
@@ -1,49 +1,80 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { v4 } from 'uuid';
 
-function NewKeg(){
+function NewKeg(event) {
+  // name: "Sour Flower",
+  // brand: "Culmination",
+  // type: "Gose & Sour",
+  // price: "6",
+  // ibu: "5",
+  // abv: "5.5",
+  // description: "Sour blonde ale with raspberry & lime zest",
+  // pints: "124"
+
+  event.preventDefault();
+  props onAddingBeer({name: Name.value, brand: Brand.value, type: Type.value, price: Price.value, ibu: IBU.value, abv: ABV.value, description: Description.value, pints: Pints.value, id: v4() })
+
+  Name.value = '';
+  Brand.value=  '';
+  Type.value=  '';
+  Price.value=  '';
+  IBU.value=  '';
+  ABV.value = '';
+  Description.value = '';
+  Pints.value = '124';
+
   return (
     <div>
       <h1>Add a beer to the list:</h1>
-      <form>
+      <form onSubmit>
         <input
           type='text'
           id='name'
+          ref={(input) => {Name = input;}}
           placeholder='Name'/>
 
         <input
           type='text'
           id='brand'
+          ref={(input) => {Brand = input;}}
           placeholder='Brand'/>
 
         <input
           type='text'
           id='type'
+          ref={(input) => {Type = input;}}
           placeholder='Type'/>
 
 
         <input
           type='number'
           id='price'
+          ref={(input) => {Price = input;}}
           placeholder='Price'/>
 
         <input
           type='number'
           id='ibu'
+          ref={(input) => {IBU = input;}}
           placeholder='IBU'/>
 
         <input
           type='number'
           id='abv'
+          ref={(input) => {ABV = input;}}
           placeholder='ABV'/>
 
         <textarea
           id='description'
+          ref={(input) => {Description = input;}}
           placeholder='Beer description.'/>
 
         <input
           type='number'
           id='pints'
-          placeholder='ABV'/>
+          ref={(input) => {Pints = input;}}
+          placeholder='Pints'/>
 
         <button type='submit'>Add new beer</button>
       </form>
@@ -53,11 +84,3 @@ function NewKeg(){
 
 export default NewKeg;
 
-name: "Sour Flower",
-brand: "Culmination",
-type: "Gose & Sour",
-price: "6",
-ibu: "5",
-abv: "5.5",
-description: "Sour blonde ale with raspberry & lime zest",
-pints: "124"
